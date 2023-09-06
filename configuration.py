62839cc608bf3e902d24053986b848dc521cd24c
sivokonnatalie <141836184+sivokonnatalie@users.noreply.github.com>
2023-09-06 07:45:46 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://fc4e78a1-53ad-40ba-afc8-6f3a7b7a62d9.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Наташка",
+    "lastName": "Милашка",
+    "address": "Сдаст питон с 1 раза",
+    "metroStation": 115,
+    "phone": "+79991112233",
+    "rentTime": 2,
+    "deliveryDate": "2023-09-05",
+    "comment": "Захвати винца",
+    "color": [
+        "BLACK"
+    ]
+}
